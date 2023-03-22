173082847935038e7b91df86fdf09c90ffb354af
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2023-03-23 01:13:02 +0530

    fix: Error of reusing variable name

--- cleaners/processDataset.js
+++ cleaners/processDataset.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
+//"", "package.json"
 function loadDatasetFiles(basedir, file) {
-  return fs
-    .readdirSync(basedir, { withFileTypes: true })
-    .filter((folder) => folder.isDirectory())
-    .map(({ name }) => {
-      let relativePath = path.join(name, file);
-      let file = fs.readFileSync(path.join(basedir, relativePath)).toString();
-      return { relativePath, file };
-    });
+  return (
+    fs
+      .readdirSync(basedir, { withFileTypes: true })
+      .filter((folder) => folder.isDirectory())
+      //folder name: xyz
+      .map(({ name }) => {
+        // xyz/package.json
+        let relativePath = path.join(name, file);
+        //
+        let fileData = fs
+          .readFileSync(path.join(basedir, relativePath))
+          .toString();
+        return { relativePath, file: fileData };
+      })
+  );
 }
 
 function cleanDataset(
@@ -24,9 +32,10 @@ function cleanDataset(
     file: cleaningFunction(file),
     ...rest,
   }));
-  for (let { relativePath, packageJson } of cleanDataset) {
+  for (let { relativePath, file } of cleanDataset) {
+    // cleaned_dataset/javaScript/xyz/package.json
     let filePath = path.join(cleanDatasetPath, relativePath);
-    let fileDir = path.dirname(packageJsonPath);
+    let fileDir = path.dirname(filePath);
     if (!fs.existsSync(fileDir)) fs.mkdirSync(fileDir, { recursive: true });
     fs.writeFileSync(filePath, file);
   }
